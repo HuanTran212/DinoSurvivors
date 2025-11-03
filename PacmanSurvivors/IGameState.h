30f57b673040536c00fc065c72c9afd62b4edb57
Trần Minh Huấn <huantranhaha212@gmail.com>
2025-11-03 15:40:46 +0700

    tạo lớp Interface Game State

---
+++ PacmanSurvivors/IGameState.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include <SFML/Graphics.hpp>
+
+class IGameState
+{
+public:
+	virtual ~IGameState() = default;
+	// Xử lý sự kiện đầu vào
+	virtual void processInput(sf::RenderWindow& window) = 0;
+	// Cập nhật trạng thái game
+	virtual void update(float dt) = 0;
+	// Vẽ nội dung lên cửa sổ
+	virtual void render(sf::RenderWindow& window) = 0;
+
+protected:
+	IGameState() = default;
+};
