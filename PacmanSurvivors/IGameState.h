878efa5843ffd9b42c366a2df138149cd57eb935
HuynhGiaBao <gia797978@gmail.com>
2025-11-03 22:19:10 +0700

    hoanthanh_MainMenuState

---
+++ PacmanSurvivors/IGameState.h
@@ -0,0 +1,16 @@
+﻿// IGameState.h
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class Game; // Khai báo trước để tránh vòng lặp include
+
+class IGameState
+{
+public:
+    virtual ~IGameState() {} // Hủy ảo
+
+    virtual void HandleInput(Game& game, sf::Event& event) = 0; // Xử lý phím
+    virtual void Update(Game& game, float dt) = 0;              // Cập nhật logic
+    virtual void Draw(Game& game, sf::RenderWindow& window) = 0;// Vẽ màn hình
+};
+#pragma once
---
+++ PacmanSurvivors/MainMenuState.h
@@ -0,0 +1,19 @@
+// MainMenuState.h
+#pragma once
+#include "IGameState.h"
+#include <SFML/Graphics.hpp>
+
+class MainMenuState : public IGameState
+{
+private:
+    sf::Font m_font;
+    sf::Text m_titleText;
+    sf::Text m_pressStartText;
+
+public:
+    MainMenuState();
+    void HandleInput(Game& game, sf::Event& event) override;
+    void Update(Game& game, float dt) override;
+    void Draw(Game& game, sf::RenderWindow& window) override;
+};
+#pragma once
